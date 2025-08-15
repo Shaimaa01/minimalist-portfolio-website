d727556a4055b15c4b2956be3887fc63ce3b5e0a
Shaimaa01 <shisko939@gmail.com>
2025-08-15 11:49:16 +0300

    feat: add initial design system assets (colors & fonts)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ function App() {
   // const unusedVariable = "hello"; // This is a linting error
 
   // This has formatting errors (extra spaces, bad quotes, wrong class order)
-  return <h1 className={"text-3xl font-bold underline"}>Hello world!</h1>;
+  return<>
+  <h1 className={"text-SlightlyDesaturatedCyan font-IbarraRealNova underline"}>Hello world!</h1>
+  <h1 className={"text-3xl font-PublicSans underline"}>Hello world!</h1>
+  </> ;
 }
 
 export default App;
